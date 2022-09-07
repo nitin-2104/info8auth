1214a24db92f529b39b46ce4189c82da949ded0c
Nitin Kumar Singh <nitinkumars607@gmail.com>
2022-09-07 12:48:03 +0530

    authentication

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -43,33 +43,3 @@ class ProductController extends Controller
     }
 
 }
-
-   //  public function create(array $input)
-   //  {
-   //      Validator::make($input, [
-   //          'f_name' => ['required', 'string', 'max:250'],
-   //          'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-   //          'password' => $this->passwordRules(),
-   //          'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
-   //          'phone_number' => ['required', 'string', 'max:250'],
-   //          'address' => ['required', 'string', 'max:250'],
-   //          'qualification' => ['required', 'string', 'max:250'],
-   //          'gender' => ['required', 'string', 'max:250'],
-
-
-   //      ])->validate();
-
-   //      return User::create([
-   //          'name'=> $input['f_name'],
-   //          'f_name' => $input['f_name'],
-   //          'email' => $input['email'],
-   //          'password' => Hash::make($input['password']),
-   //          'address' => $input['address'],
-   //          'qualification' => $input['qualification'],
-   //          'phone_number' => $input['phone_number'],
-   //          'gender' => $input['gender'],
-
-   //      ]);
-   //  }
-
-
